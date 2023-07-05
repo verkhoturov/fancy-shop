09796b906911aef4bc6d841d389ba04847a419ac
Nina Feoktistova <ninofeoktistova@gmail.com>
2023-07-05 23:44:36 +0700

    legal documents

---
+++ components/terms-conditions/index.tsx
@@ -0,0 +1,248 @@
+export const Terms = () => {
+    return (
+        <section className="section legal">
+            <div className="legal__buttons">
+                <ul className="legal__buttons-list">
+                    <li className="legal__buttons-item">
+                        <a className="legal__buttons-link legal__buttons-link--active" href="#">
+                            Terms & Conditions
+                        </a>
+                    </li>
+                    <li className="legal__buttons-item">
+                        <a className="legal__buttons-link" href="#">
+                            Privacy policy
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div className="legal__content">
+                <h1 className="legal__title">Terms & conditions</h1>
+                <div className="legal__chapter">
+                    <h2 className="legal__subtitle">Price</h2>
+                    <p className="legal__desc">
+                        The shipping cost depends on the weight and size of the product, and its
+                        price changes accordingly, therefore, it will be calculated at the time of
+                        purchase. And for the Canary Islands and other countries before consulting
+                        as they may vary depending on the products.
+                    </p>
+                </div>
+                <div className="legal__chapter">
+                    <h2 className="legal__subtitle">Shipments:</h2>
+                    <p className="legal__desc">
+                        The purchased products will be delivered to the person and address indicated
+                        in the order.
+                    </p>
+                    <p className="legal__desc">
+                        By default, the invoices will be sent to the email address indicated by the
+                        client at the time of registration in Uptown Supply Co SL
+                        (www.groundedgenetics.com).
+                    </p>
+                    <p className="legal__desc">
+                        On Saturdays, Sundays, and holidays there will be no departure or delivery
+                        of orders.
+                    </p>
+                    <p className="legal__desc">
+                        However, its delay will not imply either cancellation of the order or any
+                        compensation.
+                    </p>
+                    <p className="legal__desc">
+                        Once the order leaves our facilities, the transport company becomes
+                        responsible for any incident, be it breakage, incidents with packages,
+                        delays, etc.
+                    </p>
+                    <p className="legal__desc">
+                        For shipments outside of Spain and Portugal, consult us and for the Balearic
+                        Islands, Ceuta, Melilla, and the Canary Islands, also consult us, since the
+                        shipping rates are different.
+                    </p>
+                    <p className="legal__desc">
+                        We do not know the delivery times of the transport company, nor do we know
+                        if they call by phone before making the delivery, each area is different,
+                        and each delivery person is different.{' '}
+                    </p>
+                    <p className="legal__desc">
+                        We can help you by giving you the telephone number of the transport
+                        company’s office and the shipping number of your order, so that you can get
+                        in touch with them and agree on the delivery, it is the only thing we can do
+                        for your shipment.
+                    </p>
+                </div>
+                <div className="legal__chapter">
+                    <h2 className="legal__subtitle">2. Delivery times</h2>
+                    <dl className="legal__option-list">
+                        <dt className="legal__option">We have a 24-hour service:</dt>
+                        <dd className="legal__result">All Spain Peninsula.</dd>
+                        <dt className="legal__option">We have a 48-hour service:</dt>
+                        <dd className="legal__result">Balearic Islands.</dd>
+                    </dl>
+                    <p className="legal__desc">Normally sent 24 hours in 90% of cases.</p>
+                    <p className="legal__desc">
+                        As in all transport companies sometimes they do not meet the delivery
+                        deadline, if you call us, we can give you all the information so that you
+                        can get in touch with them. And we will try to help you.
+                    </p>
+                    <p className="legal__desc">Your satisfaction is a priority for us.</p>
+                </div>
+                <div className="legal__chapter">
+                    <h2 className="legal__subtitle">3. Return and cancellation policies</h2>
+                </div>
+                <div className="legal__chapter">
+                    <h3 className="legal__subtitle">3.1 Return of merchandise:</h3>
+                    <p className="legal__desc">
+                        Our policy, according to current regulations in Spain, is to grant you any
+                        right to return any product purchased from Uptown Supply Co SL
+                        (www.groundedgenetics.com). You have a period of 14 days from the date of
+                        receipt.
+                    </p>
+                    <p className="legal__desc">
+                        For the return it is essential that the product has its original packaging,
+                        that it is intact, just as we sent it to you at the time.
+                    </p>
+                    <p className="legal__desc">
+                        Once returned, within 3 days of receiving it, we can refund the money, or a
+                        discount voucher, as you prefer.{' '}
+                    </p>
+                    <p className="legal__desc">
+                        When a return is made, we can find these two cases:
+                    </p>
+                    <ul className="legal__list">
+                        <li className="legal__item">
+                            If you return an item because you ordered it wrong, you don’t want it,
+                            or you don’t like it. You should bear the cost of return shipping.
+                        </li>
+                        <li className="legal__item">
+                            On the other hand, if we have sent you a product that does not
+                            correspond or is defective, Uptown Supply Co SL
+                            (www.groundedgenetics.com) will bear the shipping costs. And without any
+                            hassle for the customer. Giving you all the facilities for your return,
+                            such as picking up the package at your home, etc.
+                        </li>
+                    </ul>
+                    <p className="legal__desc">
+                        In cases where you classify a product as “defective”, please check all
+                        products before making your return; Returned products that are in good
+                        working order will be returned to the customer and a round trip shipping
+                        charge will be made to the customer.
+                    </p>
+                </div>
+                <div className="legal__chapter">
+                    <h3 className="legal__subtitle">3.2 Return Conditions:</h3>
+                    <ul className="legal__list">
+                        <li className="legal__item">
+                            We are not responsible for returns that are not properly packaged, for
+                            example: If you want to return a product and you send the product
+                            without packing it in another box, the transport company will stick a
+                            shipping sticker on it and that product will be unusable for subsequent
+                            sale. In addition to insufficient packaging, the product will most
+                            likely be damaged.
+                        </li>
+                        <li className="legal__item">We cannot accept these types of returns.</li>
+                        <li className="legal__item">
+                            We are not responsible for customs fees or tariffs, etc. Generated by
+                            customs.
+                        </li>
+                        <li className="legal__item">
+                            All products have to be sent to us with their original packaging.
+                        </li>
+                        <li className="legal__item">
+                            All souvenir products are sold for decorative and collectable purposes.
+                            Uptown Supply Co SL (www.groundedgenetics.com) is not responsible for
+                            the use of these products.
+                        </li>
+                    </ul>
+                    <p className="legal__desc">
+                        * Keep in mind that we cannot accept returns of unsealed items that can be
+                        reproduced or copied (DVDs, CDs, etc), shipments without an incidence number
+                        abroad or without their original packaging.
+                    </p>
+                </div>
+                <div className="legal__chapter">
+                    <h2 className="legal__subtitle">Cancel order:</h2>
+                    <p className="legal__desc">
+                        If the cancellation of the order arrives before the departure of
+                        merchandise, the client will not have any expenses. In the case of a
+                        cancellation after the order has been dispatched, the customer will be
+                        responsible for the shipping costs, as well as the costs of returning this
+                        same order.
+                    </p>
+                    <p className="legal__desc">
+                        This merchant agrees not to allow any transaction that is illegal, or is
+                        considered by the credit card brands or the acquiring bank, that may or has
+                        the potential to damage their goodwill or negatively influence them. The
+                        following activities are prohibited under the card brand programs: the sale
+                        or offer of a product or service that is not in full compliance with all
+                        applicable laws to the Buyer, Issuing Bank, Merchant, Cardholder, or cards.
+                    </p>
+                    <p className="legal__desc legal__desc--additional">
+                        In addition, the following activities are also explicitly prohibited:
+                    </p>
+                    <ul className="legal__list">
+                        <li className="legal__item">
+                            Sell or send cannabis seeds outside of Spain.
+                        </li>
+                        <li className="legal__item">Sale of tobacco.</li>
+                    </ul>
+                    <strong className="legal__warning">
+                        IT IS NOT PERMITTED TO SELL OR SEND CANNABIS SEEDS OUTSIDE SPAIN. The sale
+                        and shipment of cannabis seeds to the United States and Canada is not
+                        permitted.{' '}
+                    </strong>
+                </div>
+                <div className="legal__chapter">
+                    <h2 className="legal__subtitle">Legal Notice</h2>
+                    <p className="legal__desc">Uptown Supply Co SL, (www.groundedgenetics.com)</p>
+                    <p className="legal__desc">
+                        Seeds are not commercial varieties and are not intended for agricultural
+                        crops. These are amateur varieties according to Law 30/2006, of July 26, on
+                        seeds and nursery plants and plant genetic resources. The seeds are sold for
+                        the personal use of those over 18 years of age only. We market cannabis
+                        seeds as items intended for collecting or genetic preservation, with the
+                        specific restriction that they are not used for purposes that violate the
+                        regulations.
+                    </p>
+                    <p className="legal__desc">
+                        Cannabis seeds are not controlled by the 1961 Single Convention on Narcotic
+                        Drugs, signed in New York on March 30, 1961, modified by the Geneva Protocol
+                        of March 25, 1972 and ratified by the Spanish State.
+                    </p>
+                    <p className="legal__desc">
+                        The Single Convention establishes which part or parts of the Cannabis Sativa
+                        L. plant are controlled; the flowering or fruiting tops of the cannabis
+                        plant, as well as the resin obtained from the cannabis plant, but expressly
+                        excludes seeds. Seeds are excluded from the legal concept of cannabis and
+                        are not included in their Annex Lists.
+                    </p>
+                    <p className="legal__desc">
+                        The seeds of the Cannabis plant lack the active ingredient Delta-9
+                        tetrahydrocannabinol (THC).
+                    </p>
+                    <p className="legal__desc">
+                        Therefore, according to the Single Convention, the seeds of the cannabis
+                        plant are not controlled and cannot be classified as narcotic drugs.
+                    </p>
+                    <p className="legal__desc">
+                        In Spain, the sale, possession and cultivation of cannabis seeds for
+                        personal use does not constitute a crime. The Spanish Penal Code
+                        criminalizes the cultivation of cannabis in order to promote, favor or
+                        facilitate its consumption by third parties.
+                    </p>
+                    <p className="legal__desc">
+                        National legislation applicable to cannabis seeds differs greatly between
+                        different countries. In some, the germination and cultivation of cannabis
+                        for personal use, and even the mere possession of the seeds, can constitute
+                        a criminal offense. Uptown Supply Co SL (www.groundedgenetics.com) does not
+                        want to induce anyone to break the law in force in any country.
+                    </p>
+                    <p className="legal__desc">
+                        For this reason, we ask non-resident buyers in Spain to inform themselves
+                        about the applicable local legislation. Uptown Supply Co SL
+                        (www.groundedgenetics.com) encourages its customers to act responsibly and
+                        disclaims any responsibility for potential legal problems for buyers, and
+                        will not make refunds for seeds seized or rejected by carriers or customs in
+                        the line of duty.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
