88dc0dacec4bc8ff6320c6a9e3552e2261b614c9
Nina Feoktistova <ninofeoktistova@gmail.com>
2023-07-06 03:21:45 +0700

    delivery

---
+++ pages/delivery.tsx
@@ -0,0 +1,10 @@
+import { Page } from '../components/page';
+import { Shipping } from '../components/shipping-returns';
+
+export default function Home() {
+    return (
+        <Page>
+            <Shipping />
+        </Page>
+    );
+}
