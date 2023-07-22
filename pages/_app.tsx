9b5a66794c9881b3e79946d6580beb0408db2ffd
MuradWeb <78862236+MuradWeb@users.noreply.github.com>
2023-07-22 22:42:15 +0300

    add progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { AgeConfirmation } from '../components/age-confirmation';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Component {...pageProps} />
+	        <NextNProgress color="#fff" />
+	        <Component {...pageProps} />
             <AgeConfirmation />
         </>
     );
