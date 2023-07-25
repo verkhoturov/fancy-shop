3549712c752a5132f13454a8dca81f88e42a9a12
Murad Akverdiyev <78862236+MuradWeb@users.noreply.github.com>
2023-07-26 00:12:33 +0300

    update

--- next.config.js
+++ next.config.js
@@ -5,7 +5,13 @@ const nextConfig = {
         NEXT_API_BACKEND_AFC: 'https://admin.groundedgenetics.com/wp-json/wp/v2/',
         WOOCOMMERCE_KEY: 'ck_9a01f2edd2d0ac5c13b30f10025faa5b7698f6fc',
         WOOCOMMERCE_SECRET: 'cs_0f95e949b58b4788a03d41f9207ec3a9c772ba44'
-    }
+    },
+    images: {
+        dangerouslyAllowSVG: true,
+        formats: ["image/webp"],
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        domains: ['admin.groundedgenetics.com'],
+    },
 };
 
 module.exports = nextConfig;
