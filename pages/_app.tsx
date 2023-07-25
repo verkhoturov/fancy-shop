90f4875a708e53846c38d7c533612fda8cd81f13
Murad Akverdiyev <78862236+MuradWeb@users.noreply.github.com>
2023-07-25 23:43:54 +0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,6 @@ import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { AgeConfirmation } from '../components/age-confirmation';
 import NextNProgress from 'nextjs-progressbar';
-import Modal from 'react-modal';
-Modal.setAppElement('#__next');
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
