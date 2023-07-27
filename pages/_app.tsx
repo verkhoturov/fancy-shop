0a486e36203ec8b6a60364fb9a10700edd99cc86
VerkhoturovIP <verkhoturovip@sibur.ru>
2023-07-27 21:29:54 +0300

    add base auth #2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import '@/styles/globals.scss';
-import { AppProps } from 'next/app';
-import { NextPageContext } from 'next';
+import type { AppProps } from 'next/app';
 import { AgeConfirmation } from '../components/age-confirmation';
 import NextNProgress from 'nextjs-progressbar';
-import { checkBasicAuth } from '../libs/basicAuth';
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <NextNProgress color="#fff" />
@@ -14,19 +12,3 @@ function MyApp({ Component, pageProps }: AppProps) {
         </>
     );
 }
-
-MyApp.getInitialProps = async (appContext: { ctx: NextPageContext }) => {
-    if (appContext.ctx.res && !checkBasicAuth(appContext.ctx.req)) {
-        appContext.ctx.res.setHeader(
-            'WWW-Authenticate',
-            'Basic realm="Access to site", charset="UTF-8"',
-        );
-        appContext.ctx.res.statusCode = 401;
-        appContext.ctx.res.end('Authentication required.');
-        return { pageProps: {} };
-    }
-
-    return { pageProps: {} };
-};
-
-export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import { useEffect } from 'react';
-import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { GetStaticProps, InferGetStaticPropsType, GetServerSideProps } from 'next';
 import { Page } from '../components/page';
 import { Slider } from '../components/slider';
 import { Services } from '../components/services';
 import { Catalog } from '../components/catalog';
 import { fetchWooCommerceProducts } from '@/libs';
 import { useProducts } from '@/store';
+import { checkBasicAuth } from '../libs/basicAuth';
 
-export default function Home(props: InferGetStaticPropsType<typeof getStaticProps>) {
+interface HomeProps {
+    categories: any[];
+    products: any[];
+};
+
+export default function Home(props: HomeProps) {
     const [addProducts, addCategories] = useProducts((state: any) => [
         state.addProducts,
         state.addCategories,
@@ -27,7 +33,14 @@ export default function Home(props: InferGetStaticPropsType<typeof getStaticProp
     );
 }
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    if (!checkBasicAuth(context.req)) {
+        context.res.setHeader('WWW-Authenticate', 'Basic realm="Access to site", charset="UTF-8"');
+        context.res.statusCode = 401;
+        context.res.end('Authentication required.');
+        return { props: {} };
+    }
+
     const wooCommerceProducts = await fetchWooCommerceProducts('products?per_page=100').catch(
         (error) => console.log(error),
     );
@@ -44,7 +57,6 @@ export const getStaticProps: GetStaticProps = async () => {
         props: {
             categories: wooCommerceCategories?.data,
             products: wooCommerceProducts?.data,
-        },
-        revalidate: 60, // regenerate page with new data fetch after 60 seconds
+        }
     };
 };
