04583f96ba9b591c710c1ade434f032cfed1578d
VerkhoturovIP <verkhoturovip@sibur.ru>
2023-07-31 20:53:44 +0300

    remove old files

--- components/age-confirmation/index.tsx
+++ components/age-confirmation/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-// import Image from 'next/image';
 
 export const AgeConfirmation = () => {
     const router = useRouter();
--- components/button/index.tsx
+++
@@ -1,3 +0,0 @@
-export const Button = () => {
-    return <button className="button" type="button"></button>;
-};
